fed431bb5998af07e181c4c157fa5120d621afff
Zhomart <zturtan@gmail.com>
2025-01-20 00:58:54 +0600

    Creating structure of project

---
+++ services/config-server/src/test/java/com/zhoma/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhoma.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/discovery/src/test/java/com/zhoma/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhoma.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
