ea6b3a1a92ecfc0e033d996acb974948aff3d379
Zhomart <zturtan@gmail.com>
2025-02-07 16:51:14 +0500

    payment service

---
+++ services/payment/src/main/java/com/zhoma/ecommerce/notification/PaymentNotificationRequest.java
@@ -0,0 +1,16 @@
+package com.zhoma.ecommerce.notification;
+
+
+import com.zhoma.ecommerce.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstname,
+        String customerLastname,
+        String customerEmail
+) {
+}
